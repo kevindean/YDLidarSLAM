8dec5a842ebab629fa33d6d59d0ea1762ff8f493
kevindean <kevindean87@gmail.com>
2020-05-05 17:51:12 -0400

    Update YDLidarClient.py

--- YDLidarClient.py
+++ YDLidarClient.py
@@ -1,78 +1,95 @@
-# the current plan is to display the point cloud within a VTK render window. In order to do this, I would like to publish
-# the point array (coming from the lidar) across a socket in order to use a callback function to acquire (if new data is
-# present from the lidar) the latest polydata to simply add it to the render window. If I were to include the YDLidar in the
-# same file as the executable function from VTK, it messes up the address acuqisition from the lidar.
-
-import socket
 import os, sys
+import socket
 import numpy as np
 import time
 import ydlidar
 
-# instantiate all variables associated with the G2B model (YdLidar)
-ydlidar.os_init()
-ports = ydlidar.lidarPortList()
-port = "/dev/ydlidar"
-for key, value in ports.items():
-    port = value
-
-laser = ydlidar.CYdLidar()
-laser.setlidaropt(ydlidar.LidarPropSerialPort, port)
-laser.setlidaropt(ydlidar.LidarPropSerialBaudrate, 230400)
-laser.setlidaropt(ydlidar.LidarPropLidarType, 1)
-laser.setlidaropt(ydlidar.LidarPropDeviceType, 0)
-laser.setlidaropt(ydlidar.LidarPropScanFrequency, 10.0)
-laser.setlidaropt(ydlidar.LidarPropSampleRate, 5)
-laser.setlidaropt(ydlidar.LidarPropFixedResolution, True)
-laser.setlidaropt(ydlidar.LidarPropInverted, True)
-laser.setlidaropt(ydlidar.LidarPropSingleChannel, False)
-laser.setlidaropt(ydlidar.LidarPropIntenstiy, True) # required for G2B, otherwise the laser will NOT turn on
-laser.setlidaropt(ydlidar.LidarPropMaxAngle, 180.0)
-laser.setlidaropt(ydlidar.LidarPropMinAngle, -180.0)
+class YdLidar():
+    def __init__(self, serial_port="/dev/ydlidar"):
+        ydlidar.os_init()
+        ports = ydlidar.lidarPortList()
+        port = serial_port
+        
+        for key, value in ports.items():
+            port = value
+        
+        self.laser = ydlidar.CYdLidar()
+        
+        self.laser.setlidaropt(ydlidar.LidarPropSerialPort, port)
+        self.laser.setlidaropt(ydlidar.LidarPropSerialBaudrate, 230400)
+        self.laser.setlidaropt(ydlidar.LidarPropLidarType, 1)
+        self.laser.setlidaropt(ydlidar.LidarPropDeviceType, 0)
+        self.laser.setlidaropt(ydlidar.LidarPropScanFrequency, 10.0)
+        self.laser.setlidaropt(ydlidar.LidarPropSampleRate, 5)
+        self.laser.setlidaropt(ydlidar.LidarPropFixedResolution, True)
+        self.laser.setlidaropt(ydlidar.LidarPropInverted, True)
+        self.laser.setlidaropt(ydlidar.LidarPropSingleChannel, False)
+        self.laser.setlidaropt(ydlidar.LidarPropIntenstiy, True) # required for G2B
+        self.laser.setlidaropt(ydlidar.LidarPropMaxAngle, 180.0)
+        self.laser.setlidaropt(ydlidar.LidarPropMinAngle, -180.0)
     
-ret = laser.initialize()
-if ret:
-    ret = laser.turnOn()
-    scan = ydlidar.LaserScan()
-    
-    time.sleep(5)
-    
-    count = 0
-    while ret and ydlidar.os_isOk() and count <= 1000:
-        r = laser.doProcessSimple(scan)
-        if r:
-            print("Scan received[", scan.stamp, "]:",
-                  scan.points.size(), "ranges is [",
-                  1.0 / scan.config.scan_time, "]Hz")
-        else :
-            print("Failed to get Lidar Data")
-            sys.exit()
-        
-        pts = []
-        
-        for point in scan.points:
-            x = point.range * np.cos(point.angle)
-            y = point.range * np.sin(point.angle)
-            z = 0.0
-            pts.append([x, y, z])
-        
-        message = np.array(pts)
+    def StopScanning(self):
+        print("Turning off & Disconnecting Laser")
+        self.laser.turnOff()
+        self.laser.disconnecting()
 
-        # Create a TCP/IP socket
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def main(maxScans=1000, serial_port="/dev/ydlidar", address='localhost', port=10000):
+    Laser = YdLidar(serial_port=serial_port)
+    
+    ret = Laser.laser.initialize()
+    if ret:
+        ret = Laser.laser.turnOn()
+        scan = ydlidar.LaserScan()
         
-        # Connect the socket to the port where the server is listening
-        server_address = ('localhost', 10000)
-        print (sys.stderr, 'connecting to %s port %s' % server_address)
-        sock.connect(server_address)
+        # Wait a few seconds
+        print("Waiting 3 seconds before acquiring data")
+        time.sleep(3)
         
-        try:
-            sock.sendall(message.tobytes())
-        finally:
-            print (sys.stderr, 'closing socket')
-            sock.close()
-        
-        count += 1
+        curScanIndex = 0
+        while ret and ydlidar.os_isOk() and curScanIndex <= maxScans:
+            r = Laser.laser.doProcessSimple(scan)
+            
+            if r:
+                pts = []
+                for point in scan.points:
+                    x = point.range * np.cos(point.angle)
+                    y = point.range * np.sin(point.angle)
+                    z = 0.0
+                    intensity = point.intensity
+                    
+                    # append calculated points to the pts list
+                    pts.append([x, y, z, intensity])
+                
+                socketMessage = np.asarray(pts)
+                
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                
+                server_address = (address, port)
+                print(sys.stderr, "Connecting to %s port %s" % server_address)
+                
+                try:
+                    sock.connect(server_address)
+                except (TimeoutError, ConnectionRefusedError) as e:
+                    print("Terminatingt\nClosing Socket")
+                    sock.close()
+                    print("Turning Off Laser")
+                    Laser.laser.turnOff()
+                    Laser.laser.disconnecting()
+                    sys.exit("Exiting Program")
+                
+                try:
+                    sock.sendall(socketMessage.tobytes())
+                finally:
+                    print(sys.stderr, 'closing socket')
+                    sock.close()
+                
+                curScanIndex += 1
+            else:
+                print(sys.stderr, "didn't acquire data,\nExiting")
+                Laser.StopScanning()
+                sys.exit()
+    
+    Laser.StopScanning()
 
-laser.turnOff()
-laser.disconnecting()
+if __name__ == "__main__":
+    main()
